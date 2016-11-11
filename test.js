65ffb629fdf82cd9fcf7e3abd4119aadfaa202da
Andrew <jiganyg@gmail.com>
2016-11-11 23:17:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>What Can JavaScript Do?</h1>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Click Me!</button>
+
+</body>
+</html>
+
