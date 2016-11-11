db9dc7e3879ce27e83f5058552bf2240c828db2c
Andrew <jiganyg@gmail.com>
2016-11-11 23:18:06 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-<body>
 
 <h1>What Can JavaScript Do?</h1>
 
-<p id="demo">JavaScript can change HTML content.</p>
+<p>JavaScript can change HTML attributes.</p>
 
-<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Click Me!</button>
+<p>In this case JavaScript changes the src (source) attribute of an image.</p>
 
-</body>
-</html>
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
+
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
 
